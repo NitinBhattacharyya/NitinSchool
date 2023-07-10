1f249315f6f206891a398c838f8e3134eb6777f3
NITIN <nitinbhattacharyya@gmail.com>
2023-07-10 21:33:07 +0530

    Update

--- src/main/java/com/example/learn_Nitin/Controller/ContactController.java
+++ src/main/java/com/example/learn_Nitin/Controller/ContactController.java
@@ -2,11 +2,15 @@ package com.example.learn_Nitin.Controller;
 
 import com.example.learn_Nitin.model.Contact;
 import com.example.learn_Nitin.service.ContactService;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -25,8 +29,12 @@ public class ContactController {
     }
 
     @RequestMapping("/contact")
-    public String displayContactPage()
+    public String displayContactPage(Model model)
     {
+        //indicating to thymeleaf that this page will hold data belong to contact object
+        //and any validation that I define inside the contact bean should be performed by the
+        //thymeleaf and spring mvc whenever the user is submitting the data
+        model.addAttribute("contact",new Contact());
         return "contact.html";
     }
 //    @RequestMapping(value="/saveMsg",method=POST)
@@ -40,9 +48,18 @@ public class ContactController {
 //        return new ModelAndView("redirect:/contact");
 //    }
 
+    //First we put @Valid so that spring knows that it has to do validations based on the annotations mentioned in contact.java
+    // followed by @ModelAttribute("contact") which says there is a model attribute with name "contact" that is binded with the object contact
+    //If there are any errors identified during this process spring will send that information under errors object
+
     @RequestMapping(value="/saveMsg",method=POST)
-    public ModelAndView saveMessage(Contact contact)
+    public ModelAndView saveMessage(@Valid @ModelAttribute("contact") Contact contact, Errors errors)
     {
+        if(errors.hasErrors())
+        {
+            log.error("Contact form validation failed due to:"+errors.toString());
+            return new ModelAndView("contact.html");
+        }
         contactService.saveMessageDetails(contact);
         return new ModelAndView("redirect:/contact");
     }
