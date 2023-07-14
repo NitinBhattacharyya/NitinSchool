421ab178b2641134135c3d52d0150f00bf87bff3
Nitin <nitinbhattacharyya@gmail.com>
2023-07-15 03:34:13 +0530

    Update

---
+++ src/main/java/com/example/learn_Nitin/constants/NitinSchoolConstants.java
@@ -0,0 +1,7 @@
+package com.example.learn_Nitin.constants;
+
+public class NitinSchoolConstants {
+    public static final String ANONYMOUS="Anonymous";
+    public static final String OPEN="Open";
+    public static final String CLOSE="Close";
+}
--- src/main/java/com/example/learn_Nitin/service/ContactService.java
+++ src/main/java/com/example/learn_Nitin/service/ContactService.java
@@ -1,13 +1,18 @@
 package com.example.learn_Nitin.service;
 
+import com.example.learn_Nitin.constants.NitinSchoolConstants;
 import com.example.learn_Nitin.model.Contact;
+import com.example.learn_Nitin.repository.ContactRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.annotation.ApplicationScope;
 import org.springframework.web.context.annotation.RequestScope;
 import org.springframework.web.context.annotation.SessionScope;
+
+import java.time.LocalDateTime;
 
 @Slf4j
 @Service
@@ -20,24 +25,33 @@ public class ContactService {
      * @param contact
      * @return boolean
      */
-    private int counter=0;
+//    private int counter=0;
 
-    public ContactService() {
-        System.out.println("Contact Service Bean initialized");
-    }
-
+//    public ContactService() {
+//        System.out.println("Contact Service Bean initialized");
+//    }
+    @Autowired
+    ContactRepository contactRepository;
     public boolean saveMessageDetails(Contact contact) {
-        boolean isSaved = true;
+        boolean isSaved = false;
+        contact.setStatus(NitinSchoolConstants.OPEN);
+        contact.setCreatedBy(NitinSchoolConstants.ANONYMOUS);
+        contact.setCreatedAt(LocalDateTime.now());
+        int result=contactRepository.saveContactMsg(contact);
+        if(result>0)
+        {
+            isSaved=true;
+        }
         //TODO - Need to persist the data into the DB table
         log.info(contact.toString());
         return isSaved;
     }
-    public int getCounter()
-    {
-        return counter;
-    }
-    public void setCounter(int counter)
-    {
-        this.counter=counter;
-    }
+//    public int getCounter()
+//    {
+//        return counter;
+//    }
+//    public void setCounter(int counter)
+//    {
+//        this.counter=counter;
+//    }
 }
