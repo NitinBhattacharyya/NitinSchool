dab8b0e93cacbdef13c097dd579239cc93f15339
Nitin <nitinbhattacharyya@gmail.com>
2023-08-28 07:09:29 +0530

    Update

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -34,4 +34,4 @@ INSERT INTO `person` (`name`,`email`,`mobile_number`,`pwd`,`role_id`,`created_at
 DELETE FROM person where email='admin@NitinSchool.com';
 
 INSERT INTO `person` (`name`,`email`,`mobile_number`,`pwd`,`role_id`,`created_at`, `created_by`)
-  VALUES ('Admin','admin@NitinSchool.com','3443434343','$2a$10$XhU4UcSxDPb5G0I0fT/CZ.Lfj2VW2fkLkUP5cOEM.xM8EzyUQXaD2', 1 ,CURDATE(),'DBA');
+  VALUES ('Admin','admin@NitinSchool.com','3443434343','$2a$12$vEl91rf6YaPq8BZh4crLp.z2J79hp6PeDiQ.K.tQRpnngJUDDVpsO', 1 ,CURDATE(),'DBA');
