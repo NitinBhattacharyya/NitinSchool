f80efdbbeecc23baae6cd6a97236c80ee49ffb00
NITIN <nitinbhattacharyya@gmail.com>
2023-09-01 12:51:48 +0530

    Update

--- src/main/java/com/example/learn_Nitin/Controller/ContactController.java
+++ src/main/java/com/example/learn_Nitin/Controller/ContactController.java
@@ -7,14 +7,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 
@@ -71,19 +69,41 @@ public class ContactController {
 //        log.info("Number of times Contact form is submitted:"+contactService.getCounter());
         return new ModelAndView("redirect:/contact");
     }
-    @RequestMapping("/displayMessages")
-    public ModelAndView displayMessages()
-    {
-        List<Contact> contactMsgs=contactService.findMsgsWithOpenStatus();
-        ModelAndView modelAndView=new ModelAndView("messages.html");
+//    @RequestMapping("/displayMessages")
+//    public ModelAndView displayMessages()
+//    {
+//        List<Contact> contactMsgs=contactService.findMsgsWithOpenStatus();
+//        ModelAndView modelAndView=new ModelAndView("messages.html");
+//        modelAndView.addObject("contactMsgs",contactMsgs);
+//        return modelAndView;
+//    }
+    @RequestMapping("/displayMessages/page/{pageNum}")
+    public ModelAndView displayMessages(Model model,
+                                        @PathVariable(name = "pageNum") int pageNum, @RequestParam("sortField") String sortField,
+                                        @RequestParam("sortDir") String sortDir) {
+        Page<Contact> msgPage = contactService.findMsgsWithOpenStatus(pageNum,sortField,sortDir);
+        List<Contact> contactMsgs = msgPage.getContent();
+        ModelAndView modelAndView = new ModelAndView("messages.html");
+        model.addAttribute("currentPage", pageNum);
+        model.addAttribute("totalPages", msgPage.getTotalPages());
+        model.addAttribute("totalMsgs", msgPage.getTotalElements());
+        model.addAttribute("sortField", sortField);
+        model.addAttribute("sortDir", sortDir);
+        model.addAttribute("reverseSortDir", sortDir.equals("asc") ? "desc" : "asc");
         modelAndView.addObject("contactMsgs",contactMsgs);
         return modelAndView;
     }
+//    @RequestMapping(value = "/closeMsg",method = GET)
+//    public String closeMsg(@RequestParam int id, Authentication authentication)
+//    {
+//        contactService.updateMsgStatus(id,authentication.getName());
+//        return "redirect:/displayMessages";
+//    }
     @RequestMapping(value = "/closeMsg",method = GET)
     public String closeMsg(@RequestParam int id, Authentication authentication)
     {
         contactService.updateMsgStatus(id,authentication.getName());
-        return "redirect:/displayMessages";
+        return "redirect:/displayMessages/page/1?sortField=name&sortDir=desc";
     }
 
 }
