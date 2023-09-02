60970ffdd5d79958ff4dbffe0402e5d2235b008b
NITIN <nitinbhattacharyya@gmail.com>
2023-09-02 12:55:30 +0530

    Update

--- src/main/java/com/example/learn_Nitin/repository/ContactRepository.java
+++ src/main/java/com/example/learn_Nitin/repository/ContactRepository.java
@@ -6,12 +6,14 @@ import com.example.learn_Nitin.rowmappers.ContactRowMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementSetter;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -69,4 +71,9 @@ public interface ContactRepository extends CrudRepository<Contact,Integer>, Pagi
     //if lets say our method argument name wasn't status but state now its different from the
     //placeholder "status" in our query. To rectify this we can use @Param("status)String state
     // now status placeholder will be mapped to status
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Contact c SET c.status=?1 WHERE c.contactId=?2")
+    int updateStatusById(String status,int id);
 }
