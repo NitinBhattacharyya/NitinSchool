a160f02c3fcc311b50bfefc08e14fd4eb8b5077f
NITIN <nitinbhattacharyya@gmail.com>
2023-10-10 19:19:26 +0530

    Update

--- ConsumeRestService/src/main/java/com/ConsumeRestService/ConsumeRestService/controller/ContactController.java
+++ ConsumeRestService/src/main/java/com/ConsumeRestService/ConsumeRestService/controller/ContactController.java
@@ -1,11 +1,15 @@
 package com.ConsumeRestService.ConsumeRestService.controller;
 
 import com.ConsumeRestService.ConsumeRestService.model.Contact;
+import com.ConsumeRestService.ConsumeRestService.model.Response;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import com.ConsumeRestService.ConsumeRestService.proxy.ContactProxy;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -15,9 +19,23 @@ public class ContactController {
     @Autowired
     ContactProxy contactProxy;
 
+    @Autowired
+    RestTemplate restTemplate;
+
     @GetMapping("/getMessages")
     public List<Contact> getMessages(@RequestParam("status")String status)
     {
         return contactProxy.getMessagesByStatus(status);
+    }
+
+    @PostMapping("/saveMsg")
+    public ResponseEntity<Response> saveMsg(@RequestBody Contact contact)
+    {
+        String url="http://localhost:8080/NitinSchool/api/contact/saveMsg";
+        HttpHeaders headers=new HttpHeaders();
+        headers.add("invocationFrom","RestTemplate");
+        HttpEntity<Contact> httpEntity=new HttpEntity<>(contact,headers);
+        ResponseEntity<Response> responseEntity=restTemplate.exchange(url, HttpMethod.POST,httpEntity,Response.class);
+        return responseEntity;
     }
 }
