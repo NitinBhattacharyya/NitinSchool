e81c4a3c2d7f94f69035d7d9ce9339cf043d19e0
NITIN <nitinbhattacharyya@gmail.com>
2023-10-10 16:48:17 +0530

    Update

--- ConsumeRestService/src/main/java/com/ConsumeRestService/ConsumeRestService/config/ProjectConfiguration.java
+++ ConsumeRestService/src/main/java/com/ConsumeRestService/ConsumeRestService/config/ProjectConfiguration.java
@@ -1,5 +1,6 @@
 package com.ConsumeRestService.ConsumeRestService.config;
 
+import feign.auth.BasicAuthRequestInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.support.BasicAuthenticationInterceptor;
@@ -7,8 +8,8 @@ import org.springframework.http.client.support.BasicAuthenticationInterceptor;
 @Configuration
 public class ProjectConfiguration {
     @Bean
-    public BasicAuthenticationInterceptor basicAuthenticationInterceptor()
+    public BasicAuthRequestInterceptor basicAuthRequestInterceptor()
     {
-        return new BasicAuthenticationInterceptor("admin@NitinSchool.com","admin");
+        return new BasicAuthRequestInterceptor("admin@NitinSchool.com","admin");
     }
 }
