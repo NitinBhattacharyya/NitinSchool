cd92b2f915df8ec1687e44749b275462e840efbe
NITIN <nitinbhattacharyya@gmail.com>
2023-10-11 01:37:28 +0530

    Update

--- src/main/java/com/example/learn_Nitin/repository/RolesRepository.java
+++ src/main/java/com/example/learn_Nitin/repository/RolesRepository.java
@@ -2,9 +2,11 @@ package com.example.learn_Nitin.repository;
 
 import com.example.learn_Nitin.model.Roles;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RepositoryRestResource(path = "roles")
 public interface RolesRepository extends JpaRepository<Roles,Integer> {
     Roles getByRoleName(String studentRole);
 }
