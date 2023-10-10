cfbcf1c546be44161b0b20a54ba99ecf1bb13a6c
NITIN <nitinbhattacharyya@gmail.com>
2023-10-10 19:49:58 +0530

    Update

--- ConsumeRestService/src/main/java/com/ConsumeRestService/ConsumeRestService/config/ProjectConfiguration.java
+++ ConsumeRestService/src/main/java/com/ConsumeRestService/ConsumeRestService/config/ProjectConfiguration.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.support.BasicAuthenticationInterceptor;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunctions;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class ProjectConfiguration {
@@ -20,5 +23,10 @@ public class ProjectConfiguration {
     {
         RestTemplateBuilder restTemplateBuilder=new RestTemplateBuilder();
         return restTemplateBuilder.basicAuthentication("admin@NitinSchool.com","admin").build();
+    }
+    @Bean
+    public WebClient webClient()
+    {
+        return WebClient.builder().filter(ExchangeFilterFunctions.basicAuthentication("admin@NitinSchool.com","admin")).build();
     }
 }
