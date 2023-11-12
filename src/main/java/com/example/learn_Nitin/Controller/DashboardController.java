a5e7bb3a6575d1539217a5c162d2d6a37ef5a700
NITIN <nitinbhattacharyya@gmail.com>
2023-11-12 19:00:06 +0530

    Update

--- src/main/java/com/example/learn_Nitin/Controller/DashboardController.java
+++ src/main/java/com/example/learn_Nitin/Controller/DashboardController.java
@@ -5,6 +5,7 @@ import com.example.learn_Nitin.repository.PersonRepository;
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
